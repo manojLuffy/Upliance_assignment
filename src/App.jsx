c764b2ab442c73d2a7f4822404bb46511204b19b
manojLuffy <manoz1994k@gmail.com>
2024-02-13 19:07:00 +0530

    route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Counter from "./components/Counter/Counter";
-import RichTextEditor from "./components/TipTapEditor/RichTextEditor";
-import UserForm from "./components/UserForm/Userform";
-import Chart from "./components/Chart";
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
 				<Routes>
-					<Route path="/counter" exact element={<Counter />} />
-					<Route path="/rich-text-editor" exact element={<RichTextEditor />} />
-					<Route path="/form" exact element={<UserForm />} />
-					<Route path="/chart" exact element={<Chart />} />
+					<Route path="/" exact element={<Counter />} />
 				</Routes>
 			</BrowserRouter>
 		</>
